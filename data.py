10afc3f224ffd6ea8ead1aee687d92bb77642f8d
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-03-18 14:44:02 +0400

    project is done

--- data.py
+++ data.py
@@ -1,20 +1,69 @@
 from prettytable import PrettyTable
 
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take "
-             "it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state "
-             "funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "The logo for Snapchat is a Bell.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "&quot;Windows NT&quot; is a monolithic kernel.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Ada Lovelace is often considered the first computer programmer.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                       "question": "MacOS is based on Linux.", "correct_answer": "False",
+                                       "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "AMD created the first consumer 64-bit processor.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "hard",
+                                       "question": "DHCP stands for Dynamic Host Configuration Port.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "The Windows ME operating system was released in the year 2000.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+                                       "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The first dual-core CPU was the Intel Pentium D.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "A Boolean value of &quot;0&quot; represents which of these words?", "correct_answer": "False",
+     "incorrect_answers": ["True"]}
 ]
 
 
@@ -31,4 +80,3 @@ def show_data():
         table.add_row(["-" * 50, "-" * 25])
 
     print(table)
-
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from question_model import Question
 from data import question_data, show_data
-from prettytable import PrettyTable
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for data in question_data:
-    question = Question(data["text"], data["answer"])
+    question = Question(data["question"], data["correct_answer"])
     question_bank.append(question)
 
+quiz = QuizBrain(question_list=question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
